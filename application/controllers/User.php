52af901e37c2938ae975c21f5ce74892f5085a64
jeehany <sriemilyani@gmail.com>
2024-05-24 09:50:06 +0800

    3

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -27,4 +27,15 @@ class User extends CI_Controller {
         );
         $this->load->view('template/main',$data);
     }
+
+    public function save()
+    {
+        $this->User_model->Save();
+        if($this->db->affected_rows()>0){
+            $this->session->set_flashdata("success","Data user berhasil disimpan");
+        }
+        redirect('user');
+    }
+
+
 }
