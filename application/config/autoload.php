6128c05b05e161fd4ec8f1db9bbcc2c0045252e2
jeehany <sriemilyani@gmail.com>
2024-05-29 16:54:24 +0800

    10

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -41,6 +41,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $autoload['packages'] = array();
 
+$autoload['libraries'] = array('database','session','form_validation');
+
+$autoload['drivers'] = array();
+
+$autoload['helper'] = array('url','pos');
+
+$autoload['config'] = array();
+
+
+$autoload['language'] = array();
+
 /*
 | -------------------------------------------------------------------
 |  Auto-load Libraries
